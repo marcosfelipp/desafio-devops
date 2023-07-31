1faaef72f90ccc9d04e6097c16665ec539c9a243
marcos <felipemarcos40@gmail.com>
2023-07-30 21:27:45 -0300

    fix: change apiURL env

--- frontend/app/src/environments/environment.prod.ts
+++ frontend/app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://a433ca5c8f47743828c31186e4bae687-2034584346.us-east-1.elb.amazonaws.com',
+  apiURL: 'http://service-backend.apps.svc.cluster.local',
   clientId: 'my-angular-app',
   clientSecret: '@321',
   obterTokenUrl: '/oauth/token'
